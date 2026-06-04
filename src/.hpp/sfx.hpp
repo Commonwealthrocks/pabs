816232ef989f92b201f70b2b349cf5366d67583e
Commonwealthrocks <204951789+Commonwealthrocks@users.noreply.github.com>
2026-06-04 16:37:17 +0200

    sfx.hpp; define sound effect calls with header

---
+++ src/.hpp/sfx.hpp
@@ -0,0 +1,13 @@
+// sfx.hpp
+// last updated: 04/06/2026
+// win32; cmake -G "Ninja" ..
+// win32; ninja
+#pragma once
+namespace sfx
+{
+    void error_sfx();
+    void success_sfx();
+    void info_sfx();
+}
+
+// end
